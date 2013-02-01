c76b2f37476cebaedb3fd7d57b55cffbec52da16
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-02-01 18:17:08 -0500

    started work on stock_spec

---
+++ spec/stock_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'spec_helper'
+require_relative '../stock'
+
+describe Stock do
+  let(:stock) {Stock.new('AAPL')}
+
+  describe ".new" do
+    it "creates a Stock object" do
+      expect(stock).to_not eq nil
+    end
+  end
+
+  describe "#name" do
+    it "has a name" do
+      expect(stock.name).to eq 'AAPL'
+    end
+  end
+
+  describe "#price" do
+    it ""
+
+end
